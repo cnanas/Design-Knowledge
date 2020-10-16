d8bd1389a339fdd257aa5d351f3d85e3abacc614
cnanas <christian@coceptwebdesigns.com>
2020-10-15 22:27:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,31 +4,36 @@ const authorTag = document.querySelector("p")
 
 const randomTag = document.querySelector("footer img")
 
-let data = []
+// let data = []
 
 
-fetch("https://api.superhi.com/api/test/quotes/random")
-    .then((response) => response.json()
-)
-    .then((jsonData) => {
-    data = jsonData
-    getQuote()
-})
+// fetch("https://api.superhi.com/api/test/quotes/random")
+//     .then((response) => response.json()
+// )
+//     .then((jsonData) => {
+//     data = jsonData
+//     getQuote()
+// })
 
 const getQuote = () => {
 
-    if (data.length > 0) {
-        const randomNumber = Math.floor(Math.random() * data.length)
-        const randomQuote = data[randomNumber]
+//     if (data.length > 0) {
+//         const randomNumber = Math.floor(Math.random() * data.length)
+//         const randomQuote = data[randomNumber]
     
-        quoteTag.innerHTML = randomQuote.quote
-        authorTag.innerHTML = randomQuote.author
-    }
+//         quoteTag.innerHTML = randomQuote.quote
+//         authorTag.innerHTML = randomQuote.author
+//     }
 
-//sample
-
+    fetch("https://api.superhi.com/api/test/quotes/random")
+        .then(response => response.json())
+        .then(jsonData => {
+            quoteTag.innerHTML = jsonData.quote
+            authorTag.innerHTML = jsonData.author   
+        })
 }
-// run the quote
+
+
 getQuote()
 
 // run getQuote on click of random image
