7865aa25f079456089e08f386c2c01b4cb4320f5
cnanas <christian@coceptwebdesigns.com>
2020-10-15 20:54:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let data = []
 fetch("quotes.json").then(function (response) {
     return response.json()
 }).then(function (jsonData) {
-    console.log(jsonData)
+    data = jsonData
 })
 
 const getQuote = function () {
