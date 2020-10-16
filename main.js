287eef07139dc207e86b4d1fc23ce94757f04f1e
cnanas <christian@coceptwebdesigns.com>
2020-10-15 20:39:22 -0700

    Files Added

---
+++ main.js
@@ -0,0 +1,43 @@
+// Promises
+// Javascrip's way of IOU's 
+// const getData = new Promise()
+// const getData = fetch('data.txt')
+// fetch('data.txt').then(() => {
+//  
+//
+//   }).catch(() => {
+//
+//   })
+
+// run this promise
+// getData()
+
+
+const quoteTag = document.querySelector("h1")
+const authorTag = document.querySelector("p")
+
+const randomTag = document.querySelector("footer img")
+
+let data = []
+
+const getQuote = function () {
+
+    if (data.length > 0) {
+        const randomNumber = Math.floor(Math.random() * data.length)
+        const randomQuote = data[randomNumber]
+    
+        quoteTag.innerHTML = randomQuote.quote
+        authorTag.innerHTML = randomQuote.author
+    }
+
+
+}
+
+// run the quote
+getQuote()
+
+// run getQuote on click of random image
+
+randomTag.addEventListener("click", () => {
+    getQuote()
+})
